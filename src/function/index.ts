1c0b6a73444bfbce7434a30291c6e373f11b0d61
KonTrax <kontraxbea+git@gmail.com>
2019-01-13 15:28:34 +0100

    switched to parameters first

--- src/function/index.ts
+++ src/function/index.ts
@@ -3,49 +3,49 @@ import { Awaitable } from '../promise'
 /**
  * Basic Function
  *
- * @param {any}   R - Return type
  * @param {any[]} A - Arguments expected
+ * @param {any}   R - Return type
  */
-export type Func <R = any, A extends any[] = any[]> =
-		| ((...args :A) => R)
+export type Func <A extends any[] = any[], R = any> =
+		(...args :A) => R
 
 /**
  * A function callable with `new`
  *
- * @param {object} I - Return instance type
  * @param {any[]}  A - Arguments expected
+ * @param {object} I - Return instance type
  */
-export type NewableFunc <I extends object = object, A extends any[] = any[]> =
-		| (new (...args :A) => I)
+export type NewableFunc <A extends any[] = any[], I extends object = object> =
+		new (...args :A) => I
 
 /**
  * A bound function
  *
  * @param {any}   This - The 'this' context of the function
- * @param {any}   R    - Return type
  * @param {any[]} A    - Arguments expected
+ * @param {any}   R    - Return type
  */
-export type BoundFunc <This, R = any, A extends any[] = any[]> =
-		| ((this :This, ...args :A) => R)
+export type BoundFunc <This, A extends any[] = any[], R = any> =
+		(this :This, ...args :A) => R
 
 /**
  * Function returning a promise
  *
  * - Compatible with `async`
  *
- * @param {any}   R - Return type
  * @param {any[]} A - Arguments expected
+ * @param {any}   R - Return type
  */
-export type AsyncFunc <R = any, A extends any[] = any[]> =
-		| Func<Promise<R>, A>
+export type AsyncFunc <A extends any[] = any[], R = any> =
+		Func<A, Promise<R>>
 
 /**
  * Function compatible with the `await` statement
  *
  * - Compatible with `await`
  *
- * @param {any}   R - Return type
  * @param {any[]} A - Arguments expected
+ * @param {any}   R - Return type
  */
-export type AwaitableFunc <R = any, A extends any[] = any[]> =
-		| Func<Awaitable<R>, A>
+export type AwaitableFunc <A extends any[] = any[], R = any> =
+		Func<A, Awaitable<R>>
