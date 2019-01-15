f9db7c9248c13c98c1a0ad92819c18cd9d753082
KonTrax <kontraxbea+git@gmail.com>
2019-01-15 07:33:01 +0100

    cleaning and added bigint to Primitive

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,24 +1,8 @@
 export * from './internal'
 
-export type NIL       = null | undefined // REVIEW: | void
+export type NIL = null | undefined // REVIEW: | void
 
-export type Primitive = string | number | boolean | symbol | NIL
-
-// REVIEW: Naming, Organisation & Usefulness
-// export namespace Type
-// {
-// 	export type Any   = NIL | Value
-
-// 	export type Value = PrimitiveValue | Complex
-
-// 	export type Primitive = PrimitiveValue | NIL
-
-// 	export type PrimitiveValue = string | number | boolean | symbol
-
-// 	/**
-// 	 * Any object types
-// 	 *
-// 	 * - Includes `Function`, `Array<any>`, etc..
-// 	 */
-// 	export type Complex = object | Function | Array<any>
-// }
+export type Primitive =
+		| string  | number | bigint
+		| boolean | symbol
+		| NIL
