73feb30ca62e79995340e913bfa8d100b88ad718
KonTrax <kontraxbea+git@gmail.com>
2019-03-23 15:31:02 +0100

    improved

--- __tests__/mapped-types/MergeProps.test.ts
+++ __tests__/mapped-types/MergeProps.test.ts
@@ -21,10 +21,10 @@ let { TRU, FAL, equals: EQ } = expect
 		}
 		type IN  = any
 	}
-	// ;{TRU = EQ<EXP, Target<IN>>()
-	// 	type EXP = never
-	// 	type IN  = unknown
-	// }
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = {}
+		type IN  = unknown
+	}
 	;{TRU = EQ<EXP, Target<IN>>()
 		type EXP = {}
 		type IN  = never
@@ -53,6 +53,104 @@ let { TRU, FAL, equals: EQ } = expect
 				| { a :1, b :1 }
 				| {       b :2, c :2 }
 				| {                   d :3 }
+	}
+
+	//=== TESTS - END ===
+
+	//----------------------------------------------------------------------------
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = { a :1, b :1 | 2, c :2, d :3 }
+		type IN  =
+				| { a :1, b  :1 }
+				| {       b ?:2, c :2 }
+				| {                    d :3 }
+	}
+
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = { a :1, b ?:1 | 2, c :2, d :3 }
+		type IN  =
+				| { a :1, b ?:1 }
+				| {       b ?:2, c :2 }
+				| {                    d :3 }
+	}
+
+	//=== TESTS - General ===
+
+	;{const eq = expect<Target<{ a  :1 }>>().equals
+		TRU = eq<{ a  :1 }>()
+		FAL = eq<{ a ?:1 }>()
+		FAL = eq<{ a  :1 | undefined }>()
+		FAL = eq<{ a ?:1 | undefined }>()
+	}
+
+	;{const eq = expect<Target<{ a ?:1 }>>().equals
+		FAL = eq<{ a  :1 }>()
+		TRU = eq<{ a ?:1 }>()
+		FAL = eq<{ a  :1 | undefined }>()
+		TRU = eq<{ a ?:1 | undefined }>()
+	}
+
+	;{const eq = expect<Target<{ a  :1 | undefined }>>().equals
+		FAL = eq<{ a  :1 }>()
+		FAL = eq<{ a ?:1 }>()
+		TRU = eq<{ a  :1 | undefined }>()
+		FAL = eq<{ a ?:1 | undefined }>()
+	}
+
+	;{const eq = expect<Target<{ a ?:1 | undefined }>>().equals
+		FAL = eq<{ a  :1 }>()
+		TRU = eq<{ a ?:1 }>()
+		FAL = eq<{ a  :1 | undefined }>()
+		TRU = eq<{ a ?:1 | undefined }>()
+	}
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = { a  :1 | 2 }
+		type IN  =
+				| { a  :1 }
+				| { a ?:2 }
+	}
+
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = { a ?:1 | 2 }
+		type IN  =
+				| { a ?:1 }
+				| { a ?:2 }
+	}
+
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = { a  :1 | 2 | undefined }
+		type IN  =
+				| { a  :1 | undefined }
+				| { a ?:2 }
+	}
+
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = { a  :1 | 2 | undefined }
+		type IN  =
+				| { a  :1 | undefined }
+				| { a ?:2 | undefined }
+	}
+
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = { a  :1 | 2 }
+		type IN  =
+				| { a  :1 }
+				| { a ?:2 | undefined }
+	}
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP, Target<IN>>()
+		type EXP = { readonly a :1 | 2, readonly b :1 | 2 }
+		type IN  =
+				| {          a  :1,          b  :1 }
+				| { readonly a  :2, readonly b ?:2 }
 	}
 
 	//=== TESTS - END ===
--- src/mapped-types/MergeProps.ts
+++ src/mapped-types/MergeProps.ts
@@ -1,13 +1,29 @@
-import { MergeKeys } from '../keys/MergeKeys'
+import { KeysI } from '../keys/Keys'
 
 /**
  * Extracts and merges props of all types in union `T`
  *
- * - Distributes
+ * - Intersects
+ *
+ * - Preserves:
+ *   - readonly
+ *   - optional _(required overrides optional)_
+ *
+ * - union:   `{ a :1 } | { a ?:2 }`
+ * - becomes: `{ a :1 | 2 }`
+ *
+ * - union:   `{ a ?:1 } | { a ?:2 }`
+ * - becomes: `{ a ?:1 | 2 }`
+ *
+ * - union:   `{ a :1 } | { readonly a :2 }`
+ * - becomes: `{ readonly a :1 | 2 }`
+ *
+ * @deprecated use `PropsI`
  *
  * @param T - union of types from which to merge props
  */
-export type MergeProps <T extends object> =
-		{ [K in MergeKeys<T>] :
-				T extends Record<K, infer V> ? V : never
+export type MergeProps <T /*extends object*/> =
+		{ [K in KeysI<T>] :
+				T extends { [P1 in K]  :infer V1} ? V1 :
+				T extends { [P2 in K] ?:infer V2} ? V2 : never
 		}
