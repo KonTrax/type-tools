83bd769d53d1e1eedad81f560559b015c5c1d62b
KonTrax <kontraxbea+git@gmail.com>
2019-03-23 15:26:15 +0100

    improved + more tests

--- __tests__/keys/KeysByValue.test.ts
+++ __tests__/keys/KeysByValue.test.ts
@@ -1,6 +1,9 @@
 import expect, {TRU, FAL} from '@ktb/type-test'
 
-import { KeysByValue as Target } from '@src'
+import {
+	KeysByValue  as Target,
+	KeysByValueU as TargetU,
+} from '@src'
 
 //==============================================================================
 //=== SETUP ===
@@ -40,13 +43,123 @@ let { TRU, FAL, equals: EQ } = expect
 
 	//=== TESTS - General ===
 
-	/// TODO: Review how to handle unions (should T be intersected?)
-	// ;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-	// 	type EXP  = 'b' | 'c'
-	// 	type IN_A = { a :1, b :1, c :2 }
-	// 						| { a :1, b :2, c :2 }
-	// 	type IN_B = 2
-	// }
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = 'a'
+		type IN_A = { a :2 }
+							| { a :2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = never
+		type IN_A = { a :1 }
+							| { a :2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = never
+		type IN_A = { a :1 }
+							| { a :1 | 2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = never
+		type IN_A = { a  :2 }
+							| { a ?:2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = never
+		type IN_A = { a ?:2 }
+							| { a ?:2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = 'a'
+		type IN_A = { a  :2 }
+							| { a  :2 | undefined }
+		type IN_B = 2 | undefined
+	}
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = 'a'
+		type IN_A = { a  :2 | undefined }
+							| { a ?:2 }
+		type IN_B = 2 | undefined
+	}
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = 'a' | 'b'
+		type IN_A = { a  :2, b :2 }
+							| { a ?:2, b :2 }
+		type IN_B = 2 | undefined
+	}
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = 'b'
+		type IN_A = { a :1, b :2 }
+							| { a :2, b :2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = 'b'
+		type IN_A = { a :1, b :2 }
+							| { a :1, b :2, c :2 }
+		type IN_B = 2
+	}
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+		type RES  = Target<IN_A, IN_B>
+		type EXP  = 'c'
+		type IN_A = { a :1, b :1, c :2, d :    2 | 3 }
+							| { a :1, b :2, c :2, d :1 | 2 }
+		type IN_B = 2
+	}
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type RES  = TargetU<IN_A, IN_B>
+		type EXP  = 'b' | 'c' | 'd'
+		type IN_A = { a :1, b :1, c :2, d :    2 | 3 }
+							| { a :1, b :2, c :2, d :1 | 2 }
+		type IN_B = 2 | 3
+	}
+
+	;{TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type RES  = TargetU<IN_A, IN_B>
+		type EXP  = 'a' | 'b'
+		type IN_A = { a :1, b :2 }
+							| { a :2, b :2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type RES  = TargetU<IN_A, IN_B>
+		type EXP  = 'b' | 'c'
+		type IN_A = { a :1, b :2 }
+							| { a :1, b :2, c :2 }
+		type IN_B = 2
+	}
 
 	//=== TESTS - END ===
 
--- src/keys/KeysByValue.ts
+++ src/keys/KeysByValue.ts
@@ -1,14 +1,21 @@
-import { PropType } from '../mapped-types/PropType'
-
 /**
- * From `T` get keys of properties whose type extends `BaseType`
+ * Extract keys of properties whose type extends `BaseType` from type `T`
  *
  * @param T        -
  * @param BaseType -
- *
- * // TODO: Review how to handle unions (should T be intersected?)
  */
-export type KeysByValue <T, BaseType> =
-		{ [K in keyof T] -?:
-				PropType<T, K> extends BaseType ? K : never
-		}[keyof T]
+export type KeysByValue <T, BaseType, K extends keyof T = keyof T> = K extends any ?
+			T[K] extends BaseType ? K : never
+		: never
+
+/**
+ * Extract keys of properties whose type extends `BaseType` from all types in union `T`
+ *
+ * - Distributes
+ *
+ * @param T        -
+ * @param BaseType -
+ */
+export type KeysByValueU <T, BaseType> = T extends any
+		? KeysByValue<T, BaseType>
+		: never
