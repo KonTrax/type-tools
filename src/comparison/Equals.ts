5cc430af956c271c9339e0f4f610a92912af850f
KonTrax <kontraxbea+git@gmail.com>
2019-03-23 05:23:58 +0100

    improved version

--- src/comparison/Equals.ts
+++ src/comparison/Equals.ts
@@ -1,4 +1,15 @@
-export type Equals <T1, T2, THEN = T1, ELSE = never> =
-		[T1 & T2] extends [never]   ? ELSE :
-		[unknown] extends [T1 | T2] ? ELSE :
-		[T1, T2]  extends [T2, T1]  ? THEN : ELSE
+/**
+ * Equals
+ *
+ * @param A -
+ * @param B -
+ */
+export type Equals <A, B, THEN = A, ELSE = never> =
+		(<T> () => T extends A ? 1 : 2) extends
+		(<T> () => T extends B ? 1 : 2) ? THEN : ELSE
+
+// Version OLD
+// export type Equals <T1, T2, THEN = T1, ELSE = never> =
+// 		[T1 & T2] extends [never]   ? ELSE :
+// 		[unknown] extends [T1 | T2] ? ELSE :
+// 		[T1, T2]  extends [T2, T1]  ? THEN : ELSE
