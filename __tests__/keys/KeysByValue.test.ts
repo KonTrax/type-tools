1a6181c204de77b41b97c00bddd7b3074fb7ccad
KonTrax <kontraxbea+git@gmail.com>
2019-03-31 07:22:37 +0200

    improved KeysByValue(U)

--- __tests__/keys/KeysByValue.test.ts
+++ __tests__/keys/KeysByValue.test.ts
@@ -3,7 +3,7 @@ import expect, {TRU, FAL} from '@ktb/type-test'
 import {
 	KeysByValue  as Target,
 	KeysByValueU as TargetU,
-} from '@src'
+} from '@src/keys/KeysByValue'
 
 //==============================================================================
 //=== SETUP ===
@@ -15,19 +15,43 @@ let { TRU, FAL, equals: EQ } = expect
 
 ;{//============================================================================
 
-	//=== TESTS - Special Types ===
+	//=== TESTS - Special Types - T ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = keyof any
+		type IN_A = any
+		type IN_B = any
+	}
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = never
+		type IN_A = unknown
+		type IN_B = any
+	}
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = never
+		type IN_A = never
+		type IN_B = any
+	}
+
+	//=== TESTS - Special Types - BaseType ===
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
 		type EXP  = 'a' | 'b'
 		type IN_A = { a :1, b :1 }
 		type IN_B = any
 	}
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
 		type EXP  = 'a' | 'b'
 		type IN_A = { a :1, b :1 }
 		type IN_B = unknown
 	}
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
 		type EXP  = never
 		type IN_A = { a :1, b :1 }
 		type IN_B = never
@@ -35,65 +59,129 @@ let { TRU, FAL, equals: EQ } = expect
 
 	//=== TESTS - General ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = never
+		type IN_A = { a  :1 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = 'a'
+		type IN_A = { a  :2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = never
+		type IN_A = { a ?:2 }
+		type IN_B = 2
+	}
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = never
+		type IN_A = { a :1 | 2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = 'a'
+		type IN_A = { a :1 & 2 }
+		type IN_B = 2
+	}
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = 'a'
+		type IN_A = { readonly a  :2 }
+		type IN_B = 2
+	}
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = never
+		type IN_A = { readonly a ?:2 }
+		type IN_B = 2
+	}
+
+	//=== TESTS - General ===
+
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = 'b'
+		type EXPU = 'b'
 		type IN_A = { a :1, b :2 }
 		type IN_B = 2
 	}
 
 	//=== TESTS - General ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = 'a'
+		type EXPU = 'a'
 		type IN_A = { a :2 }
 							| { a :2 }
 		type IN_B = 2
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = never
+		type EXPU = 'a'
 		type IN_A = { a :1 }
 							| { a :2 }
 		type IN_B = 2
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = never
+		type EXPU = never
 		type IN_A = { a :1 }
 							| { a :1 | 2 }
 		type IN_B = 2
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = never
+		type EXPU = 'a'
 		type IN_A = { a  :2 }
 							| { a ?:2 }
 		type IN_B = 2
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = never
+		type EXPU = never
 		type IN_A = { a ?:2 }
 							| { a ?:2 }
 		type IN_B = 2
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = 'a'
+		type EXPU = 'a'
 		type IN_A = { a  :2 }
 							| { a  :2 | undefined }
 		type IN_B = 2 | undefined
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = 'a'
+		type EXPU = 'a'
 		type IN_A = { a  :2 | undefined }
 							| { a ?:2 }
 		type IN_B = 2 | undefined
@@ -101,25 +189,28 @@ let { TRU, FAL, equals: EQ } = expect
 
 	//=== TESTS - General ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = 'a' | 'b'
+		type EXPU = 'a' | 'b'
 		type IN_A = { a  :2, b :2 }
 							| { a ?:2, b :2 }
 		type IN_B = 2 | undefined
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = 'b'
+		type EXPU = 'a' | 'b'
 		type IN_A = { a :1, b :2 }
 							| { a :2, b :2 }
 		type IN_B = 2
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = 'b'
+		type EXPU = 'b' | 'c'
 		type IN_A = { a :1, b :2 }
 							| { a :1, b :2, c :2 }
 		type IN_B = 2
@@ -127,9 +218,10 @@ let { TRU, FAL, equals: EQ } = expect
 
 	//=== TESTS - General ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type RES  = Target<IN_A, IN_B>
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
 		type EXP  = 'c'
+		type EXPU = 'b' | 'c'
 		type IN_A = { a :1, b :1, c :2, d :    2 | 3 }
 							| { a :1, b :2, c :2, d :1 | 2 }
 		type IN_B = 2
@@ -138,24 +230,24 @@ let { TRU, FAL, equals: EQ } = expect
 	//=== TESTS - General ===
 
 	;{TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
-		type RES  = TargetU<IN_A, IN_B>
 		type EXP  = 'b' | 'c' | 'd'
+		type EXPU = 'b' | 'c' | 'd'
 		type IN_A = { a :1, b :1, c :2, d :    2 | 3 }
 							| { a :1, b :2, c :2, d :1 | 2 }
 		type IN_B = 2 | 3
 	}
 
 	;{TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
-		type RES  = TargetU<IN_A, IN_B>
 		type EXP  = 'a' | 'b'
+		type EXPU = 'a' | 'b'
 		type IN_A = { a :1, b :2 }
 							| { a :2, b :2 }
 		type IN_B = 2
 	}
 
 	;{TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
-		type RES  = TargetU<IN_A, IN_B>
 		type EXP  = 'b' | 'c'
+		type EXPU = 'b' | 'c'
 		type IN_A = { a :1, b :2 }
 							| { a :1, b :2, c :2 }
 		type IN_B = 2
--- src/keys/KeysByValue.ts
+++ src/keys/KeysByValue.ts
@@ -1,12 +1,30 @@
 /**
  * Extract keys of properties whose type extends `BaseType` from type `T`
  *
+ * @internal
+ * @param T        -
+ * @param BaseType -
+ * @param K        -
+ */
+type _KeysByValue <T, BaseType, K extends keyof T> = K extends any
+		? T[K] extends BaseType ? K : never
+		: never
+
+// Version B
+// type _KeysByValue <T, BaseType, K extends keyof T> =
+// 		{ [P in K] -?:
+// 				T[P] extends BaseType ? P : never
+// 		}[K]
+
+/**
+ * Extract keys of properties whose type extends `BaseType` from type `T`
+ *
  * @param T        -
  * @param BaseType -
  */
-export type KeysByValue <T, BaseType, K extends keyof T = keyof T> = K extends any ?
-			T[K] extends BaseType ? K : never
-		: never
+// Version A
+export type KeysByValue <T, BaseType> =
+		_KeysByValue<T, BaseType, keyof T>
 
 /**
  * Extract keys of properties whose type extends `BaseType` from all types in union `T`
@@ -17,5 +35,5 @@ export type KeysByValue <T, BaseType, K extends keyof T = keyof T> = K extends a
  * @param BaseType -
  */
 export type KeysByValueU <T, BaseType> = T extends any
-		? KeysByValue<T, BaseType>
+		? _KeysByValue<T, BaseType, keyof T>
 		: never
