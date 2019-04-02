d6d839f016c964b467d554cbd5ca6636a00a4892
KonTrax <kontraxbea+git@gmail.com>
2019-04-02 11:54:45 +0200

    Refactored to new system + more tests

--- __tests__/mapped-types/OmitByValue.test.ts
+++ __tests__/mapped-types/OmitByValue.test.ts
@@ -1,51 +1,177 @@
 import expect, {TRU, FAL} from '@ktb/type-test'
 
-import { OmitByValue as Target } from '@src/mapped-types/OmitByValue'
+import {
+	OmitByValue  as Target,
+	OmitByValueU as TargetU,
+} from '@src/mapped-types/OmitByValue'
+
+import { KeysU } from '@src/keys/Keys'
 
 //==============================================================================
 //=== SETUP ===
 
-let { TRU, FAL, equals: EQ } = expect
+let { TRU, FAL, equals: EQ, accepts: ACC, extends: EXT } = expect
+
+type STR = string
+type NUM = number
 
 //==============================================================================
 //=== TESTS ===
 
 ;{//============================================================================
 
-	//=== TESTS - Special Types ===
+//==============================================================================
+//=== TESTS - Special Types ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+	//=== Param A ===
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = {}
+		type IN_A = any
+		type IN_B = NUM
+
+		type RES  = Target <IN_A, IN_B>
+		type RESU = TargetU<IN_A, IN_B>
+	}
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = {}
+		type IN_A = unknown
+		type IN_B = NUM
+
+		type RES  = Target <IN_A, IN_B>
+		type RESU = TargetU<IN_A, IN_B>
+	}
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXP  = {}
+		type EXPU = never
+		type IN_A = never
+		type IN_B = NUM
+
+		type RES  = Target <IN_A, IN_B>
+		type RESU = TargetU<IN_A, IN_B>
+	}
+
+	//=== Param B ===
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
 		type EXP  = {}
 		type IN_A = { a :1, b :1 }
 		type IN_B = any
 	}
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
 		type EXP  = {}
 		type IN_A = { a :1, b :1 }
 		type IN_B = unknown
 	}
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
 		type EXP  = { a :1, b :1 }
 		type IN_A = { a :1, b :1 }
 		type IN_B = never
 	}
 
-	//=== TESTS - General ===
+//===
+//==============================================================================
+//=== TESTS - Single - General ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type EXP  = { a :1 }
-		type IN_A = { a :1, b :2 }
-		type IN_B = 2
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = { a :STR }
+		type IN_A = { a :STR, b :NUM }
+		type IN_B = NUM
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type EXP  = { a :1, b :1 }
-							| { a :1 }
-		type IN_A = { a :1, b :1 }
-							| { a :1, b :2 }
-		type IN_B = 2
+//===
+//==============================================================================
+//=== TESTS - Single - Modifiers ===
+
+	//=== Normal ===
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = {}
+		type IN_A = { a :1 }
+		type IN_B = 1
 	}
 
-	//=== TESTS - END ===
+	//=== Optional ===
 
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = { a ?:1 }
+		type IN_A = { a ?:1 }
+		type IN_B = 1
+	}
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = {}
+		type IN_A = { a ?:1 }
+		type IN_B = 1 | undefined
+	}
+
+	//=== Readonly ===
+
+	;{TRU = EQ<EXP, Target <IN_A, IN_B>>()
+		TRU = EQ<EXP, TargetU<IN_A, IN_B>>()
+		type EXP  = {}
+		type IN_A = { readonly a :1 }
+		type IN_B = 1
+	}
+
+//===
+//==============================================================================
+//=== TESTS - Union - General ===
+
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXP  = { a :STR, b :STR | NUM }
+		type EXPU = { a :STR, b :STR }
+							| { a :STR }
+		type IN_A = { a :STR, b :STR }
+							| { a :STR, b :NUM }
+		type IN_B = NUM
+
+		type RES  = Target <IN_A, IN_B>
+		type RESU = TargetU<IN_A, IN_B>
+	}
+
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXP  = { b :STR | NUM }
+		type EXPU = {}
+							| { b :NUM }
+		type IN_A = { a :STR, b :STR }
+							| { a :STR, b :NUM }
+		type IN_B = STR
+
+		type RES  = Target <IN_A, IN_B>
+		type RESU = TargetU<IN_A, IN_B>
+	}
+
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		// TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		TRU = EQ<KeysU<EXPU>, KeysU<TargetU<IN_A, IN_B>>>()
+		TRU = ACC<EXPU, TargetU<IN_A, IN_B>>()
+		TRU = EXT<EXPU, TargetU<IN_A, IN_B>>()
+
+		type EXP  = {}
+		type EXPU = {}
+		type IN_A = { a :STR, b :STR }
+							| { a :STR, b :NUM }
+		type IN_B = NUM | STR
+
+		type RES  = Target <IN_A, IN_B>
+		type RESU = TargetU<IN_A, IN_B>
+	}
+
+//===
 }//=============================================================================
+
+//===
+//==============================================================================
--- src/mapped-types/OmitByValue.ts
+++ src/mapped-types/OmitByValue.ts
@@ -1,12 +1,25 @@
 import { KeysByValue } from '../keys/KeysByValue'
 import { OmitByKey } from './OmitByKey'
 
+// REVIEW: Should this omit by value-type or prop-type?
+
 /**
  * From `T` omit properties whose type extends `BaseType`
  *
  * @param T        -
  * @param BaseType -
  */
-export type OmitByValue <T, BaseType> = T extends any
-		? OmitByKey<T, KeysByValue<T, BaseType>>
+export type OmitByValue <T, BaseType> =
+		OmitByKey<T, KeysByValue<T, BaseType>>
+
+/**
+ * From `T` omit properties whose type extends `BaseType`
+ *
+ * - Distributes
+ *
+ * @param T        -
+ * @param BaseType -
+ */
+export type OmitByValueU <T, BaseType> = T extends any
+		? OmitByValue<T, BaseType>
 		: never
