b9eabbba8b70d6feb78956569665a4a0cf8e6298
KonTrax <kontraxbea+git@gmail.com>
2019-04-02 11:48:29 +0200

    refactored to new system + more tests

--- __tests__/mapped-types/OmitByKey.test.ts
+++ __tests__/mapped-types/OmitByKey.test.ts
@@ -1,6 +1,13 @@
 import expect, {TRU, FAL} from '@ktb/type-test'
 
-import { OmitByKey as Target } from '@src/mapped-types/OmitByKey'
+import {
+	OmitByKey  as Target,
+	OmitByKeyU as TargetU,
+} from '@src/mapped-types/OmitByKey'
+
+import { KeysU } from '@src/keys/Keys'
+
+// import * as TT from '@src'
 
 //==============================================================================
 //=== SETUP ===
@@ -14,46 +21,193 @@ let { TRU, FAL, equals: EQ } = expect
 
 	//=== TESTS - Special Types ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type EXP  = {}
-		type IN_A = { a :1, b :1 }
-		type IN_B = any
+	/// CASE: xxx
+	;{TRU = EQ<EXP, Target <IN, keyof IN>>()
+		TRU = EQ<EXP, TargetU<IN, keyof IN>>()
+		type EXP = {}
+		type IN  = any
 	}
-	// ;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-	// 	type EXP  = {}
-	// 	type IN_A = { a :1, b :1 }
-	// 	type IN_B = unknown
-	// }
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type EXP  = { a :1, b :1 }
-		type IN_A = { a :1, b :1 }
-		type IN_B = never
+	/// CASE: xxx
+	;{TRU = EQ<EXP, Target <IN, keyof IN>>()
+		TRU = EQ<EXP, TargetU<IN, keyof IN>>()
+		type EXP = unknown
+		type IN  = unknown
+	}
+	/// CASE: xxx
+	;{TRU = EQ<EXP,  Target <IN, keyof IN>>()
+		TRU = EQ<EXPU, TargetU<IN, keyof IN>>()
+		type EXP  = {}
+		type EXPU = never
+		type IN   = never
 	}
 
 	//=== TESTS - General ===
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type EXP  = { a :1 }
-		type IN_A = { a :1, b :1 }
-		type IN_B = 'b'
+	/// CASE: xxx
+	;{TRU = EQ<EXP, Target <IN, keyof IN>>()
+		TRU = EQ<EXP, TargetU<IN, keyof IN>>()
+		type EXP = {}
+		type IN  = {}
+	}
+	/// CASE: xxx
+	;{TRU = EQ<EXP, Target <IN, keyof IN>>()
+		TRU = EQ<EXP, TargetU<IN, keyof IN>>()
+		type EXP = {}
+		type IN  = object
+	}
+	/// CASE: xxx
+	;{TRU = EQ<EXP, Target <IN, keyof IN>>()
+		TRU = EQ<EXP, TargetU<IN, keyof IN>>()
+		type EXP = {}
+		type IN  = Object
 	}
 
-	;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-		type EXP  = { a :1 }
-							| { a :2 }
-		type IN_A = { a :1, b :1 }
-							| { a :2, b :2 }
-		type IN_B = 'b'
+	//=== TESTS - General ===
+
+	/// CASE: xxx
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXP  = { b :12 | 22 }
+		type EXPU = IN_A
+		type IN_B = never
+		type IN_A =
+				| { a :11, b :12 }
+				| {        b :22, c :23 }
 	}
 
-	//=== TESTS - Invalid ===
+	//=== TESTS - General ===
 
-	// ;{TRU = EQ<EXP, Target<IN_A, IN_B>>()
-	// 	type EXP  = { a :1 }
-	// 	type IN_A = { a :1, b :1 }
-	// 						| { a :2}
-	// 	type IN_B = 'b'
-	// }
+	/// CASE: xxx
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXP  = {}
+		type EXPU = { a :11 } | { c :23 }
+		type IN_B = 'b'
+		type IN_A =
+				| { a :11, b :12 }
+				| {        b :22, c :23 }
+	}
+
+	/// CASE: xxx
+	;{TRU = EQ<EXP,  Target <IN_A, IN_B>>()
+		TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXP  = { a :11 | 21 }
+		type EXPU = { a :11 } | { a :21 }
+		type IN_B = 'b'
+		type IN_A =
+				| { a :11, b :12 }
+				| { a :21, b :22 }
+	}
+
+	//=== TESTS - END ===
+
+}//=============================================================================
+
+//==============================================================================
+//=== TESTS - TargetU only ===
+
+;{//============================================================================
+
+	//=== TESTS - General ===
+
+	/// CASE: xxx
+	;{TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXPU = {} | { c :23 }
+		type IN_B = 'a' | 'b'
+		type IN_A =
+				| { a :11, b :12 }
+				| {        b :22, c :23 }
+	}
+
+	//=== TESTS - General ===
+
+	/// CASE: xxx
+	;{TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXPU = {}
+		type IN_B = 'a'
+		type IN_A = { a :1 }
+	}
+	/// CASE: xxx
+	;{TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXPU = {}
+		type IN_B = 'a'
+		type IN_A = { a ?:1 }
+	}
+
+	//=== TESTS - General ===
+
+	/// CASE: xxx
+	;{TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXPU = {} | { b :3 }
+		type IN_B = 'a'
+		type IN_A =
+				| { a ?:1 }
+				| { a  :2, b :3 }
+	}
+
+	/// CASE: xxx
+	;{TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXPU = {} | { b ?:3 }
+		type IN_B = 'a'
+		type IN_A =
+				| { a ?:1 }
+				| { a  :2, b ?:3 }
+	}
+
+	/// CASE: xxx
+	;{TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		type EXPU = {} | { b ?:3 }
+		type IN_B = 'a'
+		type IN_A =
+				| {}
+				| { a  :2, b ?:3 }
+	}
+
+	//=== TESTS - General ===
+
+	/// CASE: xxx
+	;{TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		TRU = EQ<KeysU<EXPU>, KeysU<TargetU<IN_A, IN_B>>>()
+		type EXPU = {} | { b :3 }
+		type IN_B = 'a'
+		type IN_A =
+				| { a ?:1 }
+				| { a  :2, b :3 }
+	}
+
+	/// CASE: xxx
+	;{TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		TRU = EQ<KeysU<EXPU>, KeysU<TargetU<IN_A, IN_B>>>()
+		type EXPU = {} | { b ?:3 }
+		type IN_B = 'a'
+		type IN_A =
+				| { a ?:1 }
+				| { a  :2, b ?:3 }
+	}
+
+	//=== TESTS - General ===
+
+	/// CASE: xxx
+	;{
+		// TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		TRU = EQ<KeysU<EXPU>, KeysU<TargetU<IN_A, IN_B>>>()
+		type EXPU = {} | {}
+		type IN_B = 'a' | 'b'
+		type IN_A =
+				| { a ?:1 }
+				| { a  :2, b :3 }
+	}
+
+	/// CASE: xxx
+	;{
+		// TRU = EQ<EXPU, TargetU<IN_A, IN_B>>()
+		TRU = EQ<KeysU<EXPU>, KeysU<TargetU<IN_A, IN_B>>>()
+		type EXPU = {} | {}
+		type IN_B = 'a' | 'b'
+		type IN_A =
+				| { a ?:1 }
+				| { a  :2, b ?:3 }
+	}
 
 	//=== TESTS - END ===
 
--- src/mapped-types/OmitByKey.ts
+++ src/mapped-types/OmitByKey.ts
@@ -1,9 +1,21 @@
+import { KeysU } from '../keys/Keys'
 /**
  * From `T` omit a set of properties `K`
  *
  * @param           T -
  * @param {keyof T} K -
  */
-export type OmitByKey <T, K extends keyof T> = T extends any
-		? Pick<T, Exclude<keyof T, K>>
+export type OmitByKey <T, K extends keyof T> =
+		Pick<T, Exclude<keyof T, K>>
+
+/**
+ * From `T` omit a set of properties `K`
+ *
+ * - Distributes
+ *
+ * @param           T -
+ * @param {keyof T} K -
+ */
+export type OmitByKeyU <T, K extends KeysU<T>> = T extends any
+		? OmitByKey<T, Extract<keyof T, K>>
 		: never
